d8f04922563c0195dc50cbeb69da2c74ad9ac4b4
Kevin Goldstein <kevin@sendence.com>
2017-05-24 21:47:53 +0000

    works

--- runner.sh
+++ runner.sh
@@ -3,6 +3,7 @@ export PATH="$PATH:/home/ubuntu/wallaroo-tutorial/wallaroo/machida/build"
 export PYTHONPATH="$PYTHONPATH:/home/ubuntu/wallaroo-tutorial/wallaroo/machida"
 
 WORKER_COUT=15
+echo "Starting worker: intializer, cpu:$WORKER_COUT"
 taskset -c $WORKER_COUT \
 machida \
 --application-module RecognizeInWallaroo \
@@ -16,7 +17,7 @@ for i in `seq 0 14`
 do
     sleep 3
     name="worker-$i"
-    echo "Starting worker: $name"
+    echo "Starting worker: $name, CPU: $i"
     taskset -c $i \
 	machida \
 	--application-module RecognizeInWallaroo \
@@ -25,4 +26,3 @@ do
 	--data 127.0.0.1:6001 --worker-count $WORKER_COUT --name $name \
 	--ponythreads=1 > run.log.$i 2>&1 &
 done
-
