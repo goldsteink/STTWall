a82f9b4c1f2b8bc4be935de5975122d50ff34e54
Ubuntu <ubuntu@ip-172-31-154-193.ec2.internal>
2017-05-24 19:21:03 +0000

    runs 15 instancnes

---
+++ runner.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+export PATH="$PATH:/home/ubuntu/wallaroo-tutorial/wallaroo/machida/build"
+export PYTHONPATH="$PYTHONPATH:/home/ubuntu/wallaroo-tutorial/wallaroo/machida"
+
+WORKER_COUT=15
+taskset -c $WORKER_COUT \
+machida \
+--application-module RecognizeInWallaroo \
+--in 127.0.0.1:7010 \
+--out 127.0.0.1:7002 --metrics 127.0.0.1:5001 --control 127.0.0.1:6000 \
+--data 127.0.0.1:6001 --worker-count $WORKER_COUT --topology-initializer \
+--ponythreads=1 > run.log.initializer 2>&1 &
+
+name=""
+for i in `seq 0 14`
+do
+    sleep 3
+    name="worker-$i"
+    echo "Starting worker: $name"
+    taskset -c $i \
+	machida \
+	--application-module RecognizeInWallaroo \
+	--in 127.0.0.1:7010 \
+	--out 127.0.0.1:7002 --metrics 127.0.0.1:5001 --control 127.0.0.1:6000 \
+	--data 127.0.0.1:6001 --worker-count $WORKER_COUT --name $name \
+	--ponythreads=1 > run.log.$i 2>&1 &
+done
+
