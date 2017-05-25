93f64c08517085d24fec0220ab045271060df7a4
Kevin Goldstein <kevin@sendence.com>
2017-05-25 15:26:42 +0000

    better word list

--- send.sh
+++ send.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
-/home/kgoldstein/wallaroo-tutorial/wallaroo/giles/sender/sender \
+/home/ubuntu/wallaroo-tutorial/wallaroo/giles/sender/sender \
 --host 127.0.0.1:7010 \
---file recepies.txt \
+--file files.txt \
 --batch-size 1 \
 --interval 100_000_000 \
 --messages 2000 \
