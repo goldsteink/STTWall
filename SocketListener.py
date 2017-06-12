ed7a9b2f6e2269e0b867b0bb32e81fe863358a9c
Kevin Goldstein <kevin@sendence.com>
2017-06-12 18:45:25 +0000

    A better socket listenr

---
+++ SocketListener.py
@@ -0,0 +1,55 @@
+'''
+    Simple socket server using threads
+'''
+ 
+import socket
+import sys
+from thread import *
+ 
+HOST = ''   # Symbolic name meaning all available interfaces
+PORT = 7002 # Arbitrary non-privileged port
+ 
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+#print 'Socket created'
+ 
+#Bind socket to local host and port
+try:
+    s.bind((HOST, PORT))
+except socket.error as msg:
+    print 'Bind failed. Error Code : ' + str(msg[0]) + ' Message ' + msg[1]
+    sys.exit()
+     
+
+#Start listening on socket
+s.listen(10)
+#print 'Socket now listening'
+ 
+#Function for handling connections. This will be used to create threads
+def clientthread(conn):
+    #Sending message to connected client
+     
+    #infinite loop so that function do not terminate and thread do not end.
+    while True:
+         
+        #Receiving from client
+        data = conn.recv(1024)
+        reply = 'OK...' + data
+        if not data: 
+            break
+        else:
+            print data.strip()
+     
+    #came out of loop
+    conn.close()
+ 
+#now keep talking with the client
+while 1:
+    #wait to accept a connection - blocking call
+    conn, addr = s.accept()
+    #print 'Connected with ' + addr[0] + ':' + str(addr[1])
+     
+    #start new thread takes 1st argument as a function name to be run, second is the tuple of arguments to the function.
+    start_new_thread(clientthread ,(conn,))
+ 
+s.close()
